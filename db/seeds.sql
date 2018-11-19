1f74781be6fa50d6eccc529a8df5926d8e5e59fc
RichardVargas1 <richard.vargas95@gmail.com>
2018-11-18 20:11:14 -0600

    Adding schema.sql & seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers (burger_name, devoured) VALUES
+
+("The Big Burger", false),
+("The Bigger Burger", true),
+("Texas Burger", false),
+("The Hamburguesa", true),
+("Bacon Burger", false),
+("Frisco Burger", true),
+("Double Burger", false),
+("Buffalo Burger", false),
+("Cheeseburger", true),
+("Jalapeño Burger", true);
